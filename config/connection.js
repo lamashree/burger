a5a0e1f58295e67598b5ac879edf86653c94f812
admin <admin@admins-MacBook-Pro.local>
2020-01-06 12:38:06 -0800

    make it better

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
      connection = mysql.createConnection({
-    host: "localhost",
+    host: "localhost1",
     port: 3306,
     user: "root",
     password: "yourRootPassword",
