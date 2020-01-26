e1c4ffbdc0e29b3ccca1e73af81d2d43621805fb
admin <admin@admins-MacBook-Pro.local>
2020-01-26 01:15:30 -0800

    try to deployed

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ app.set("view engine", "handlebars");
 var routes = require("./controllers/burgers_controller.js");
 app.use(routes);
 
-
-app.listen(PORT, function() {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
-  });
+app.listen(process.env.PORT || 8080) 
+// app.listen(PORT, function() {
+//     // Log (server-side) when our server has started
+//     console.log("Server listening on: http://localhost:" + PORT);
+//   });
   
